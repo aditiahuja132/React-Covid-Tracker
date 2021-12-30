78eea770756eaaaf30e617affaa3ac59646c118c
aditiahuja132 <77011231+aditiahuja132@users.noreply.github.com>
2021-12-30 16:31:37 +0530

    Final Commit

---
+++ precache-manifest.40353fe3e3c7cac5c7d82520ac80c744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289c9fcc0e79fc9a5d6e3fcb9850b278",
+    "url": "/React-Covid-Tracker/index.html"
+  },
+  {
+    "revision": "98bb3a414db2658ebab5",
+    "url": "/React-Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "ec6e8fa4a11a977680e8",
+    "url": "/React-Covid-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "98bb3a414db2658ebab5",
+    "url": "/React-Covid-Tracker/static/js/2.de011f37.chunk.js"
+  },
+  {
+    "revision": "88ab8e9bfa1a02a6d25f3aa28a50aeac",
+    "url": "/React-Covid-Tracker/static/js/2.de011f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6e8fa4a11a977680e8",
+    "url": "/React-Covid-Tracker/static/js/main.45f52d33.chunk.js"
+  },
+  {
+    "revision": "c26ecf2d5154e9765db5",
+    "url": "/React-Covid-Tracker/static/js/runtime-main.e1c9bd7c.js"
+  }
+]);
